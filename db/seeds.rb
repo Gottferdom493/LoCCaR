eaff08ca39f65e2b549794be5833bbf3dc1858a2
raphael <raphael.bichet@yahoo.com>
2022-12-17 22:27:27 +0100

    ajout du seed + création de views new pour la table bookings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Car.destroy_all
+Car.create!(marque: "BMW", modele: "Z3", adresse: "10 av Marcel Dassault", prix: 15000)
+Car.create!(marque: "Renault", modele: "Senic", adresse: "24 allée du clos fleuri", prix: 7500)
+Car.create!(marque: "Ferrari", modele: "Top", adresse: "3 rue du palace, Monaco", prix: 375050)
+# Booking.create!(marque: marque1, modele: modele1, adresse: Date.new(2022,03,15) , end_date: Date.new(2022,03,16))
