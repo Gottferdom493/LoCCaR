c588bc53a540bdb0ea0a975c0cc6044fc2a885a3
raphael <raphael.bichet@yahoo.com>
2022-12-18 12:11:44 +0100

    message de réservation ok

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -9,11 +9,13 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @car = Car.find(params[:car_id])
     @booking.car = @car
-    if @booking.save
-      redirect_to cars_path(), notice: "Comment has been added"
-    else
-      render :new
-    end
+    # if
+      @booking.save
+      # flash[:notice] = "Vous avez-bien réservé votre #{@car.marque} #{@car.modele} ! Merci pour votre confiance"
+      redirect_to new_car_booking_path(), notice: "Booking has been added"
+    # else
+      # render :new
+    # end
   end
 
   private
@@ -21,6 +23,5 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
   end
-
 
 end
