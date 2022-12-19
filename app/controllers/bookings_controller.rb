cdcea562da4faf1e5692658ca2498e59665baffd
raphael <raphael.bichet@yahoo.com>
2022-12-19 14:33:46 +0100

    ajout du reference de user à car + les has_many et belongs_to dans les models

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,19 +3,18 @@ class BookingsController < ApplicationController
   def new
     @car = Car.find(params[:car_id])
     @booking = Booking.new
+    @bookings = Booking.all
   end
 
   def create
     @booking = Booking.new(booking_params)
     @car = Car.find(params[:car_id])
     @booking.car = @car
-    # if
-      @booking.save
-      # flash[:notice] = "Vous avez-bien réservé votre #{@car.marque} #{@car.modele} ! Merci pour votre confiance"
-      redirect_to new_car_booking_path(), notice: "Booking has been added"
-    # else
-      # render :new
-    # end
+    if @booking.save!
+      redirect_to new_car_booking_path(car), notice: "Booking has been added"
+    else
+      render :new
+    end
   end
 
   private
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,6 +2,7 @@ class Car < ApplicationRecord
 
   validates :marque, :modele, :adresse, :prix, presence: true
 
+  belongs_to :user
   has_many :bookings, dependent: :destroy
 
 end
